a7bf929663c22019f5414234922787469fec337c
Tweber663 <tweber.1995@hotmail.com>
2024-09-18 15:39:42 +0200

    js, css and JS added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,4 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const buttons = document.querySelectorAll('button');
+Array.from(buttons).forEach((btn) => btn.addEventListener('click', () => { var _a; return (_a = btn.parentElement) === null || _a === void 0 ? void 0 : _a.classList.toggle('active'); }));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,2 @@
-import Stopwatch from './StopwatchWithResults.js'
-
-new Stopwatch(document.querySelector('.stopwatch'))
+const buttons = document.querySelectorAll('button'); 
+Array.from(buttons).forEach((btn) =>  btn.addEventListener('click', () => btn.parentElement?.classList.toggle('active')))
